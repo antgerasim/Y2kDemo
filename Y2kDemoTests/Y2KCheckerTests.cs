fe433c4998e04a40fb2c480134409966c858a3af
antgerasim <antgerasim@gmail.com>
2015-04-12 21:13:58 +0300

    Removed unmanaged "dragon" code.
    Added 'shimmed' DateTime object within using statement.
    
    Test passes.

--- Y2kDemoTests/Y2KCheckerTests.cs
+++ Y2kDemoTests/Y2KCheckerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Runtime.InteropServices;
+using System.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Y2kDemo;
 
@@ -14,37 +15,17 @@ namespace Y2kDemoTests
       [ExpectedException(typeof (Y2KException))]
       public void ThrowIfY2KTest()
       {
-         //arrange
-         var systemtime = new SYSTEMTIME(new DateTime(2000, 1, 1));
-         var sut = new Y2KChecker();
-         var result = SetSystemTime(ref systemtime);
-
-         Assert.IsTrue(result);
-
-         sut.ThrowIfY2K();
-      }
-
-      #region Here there be dragons
-      [StructLayout(LayoutKind.Sequential)]
-      private struct SYSTEMTIME
-      {
-
-         [MarshalAs(UnmanagedType.U2)] public short Year;
-         [MarshalAs(UnmanagedType.U2)] public short Month;
-         [MarshalAs(UnmanagedType.U2)] public short DayOfWeek;
-
-         public SYSTEMTIME(DateTime dateTime)
+         using (ShimsContext.Create())
          {
-            Year = (short) dateTime.Year;
-            Month = (short) dateTime.Month;
-            DayOfWeek = (short) dateTime.DayOfWeek;
+            var sut = new Y2KChecker();
+
+            ShimDateTime.NowGet = () => new DateTime(2000, 1, 1);
+
+            sut.ThrowIfY2K();
          }
 
       }
 
-      [DllImport("kernel32.dll", SetLastError = true)]
-      private static extern bool SetSystemTime(ref SYSTEMTIME time);
-      #endregion
    }
 
 }
